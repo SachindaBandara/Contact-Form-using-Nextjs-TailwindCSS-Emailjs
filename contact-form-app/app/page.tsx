68871268b0561d0eb4aa4a39b312ffb190033450
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-12 18:29:14 +0530

    add

--- contact-form-app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ContactForm from "@/pages/contact";
-
-export default function Home() {
-  return (
-    <main className="flex flex-col items-center px-4">
-     <ContactForm/>
-    </main>
-  );
-}
