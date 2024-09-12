72d1321bf56c581f777f624b32087fbec7249a7a
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-09-12 19:29:36 +0530

    add

---
+++ contact-form/components/SubmitBtn.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useFormStatus } from "react-dom";
+import { FaPaperPlane } from "react-icons/fa";
+
+export default function SubmitBtn() {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      type="submit"
+      className="group flex items-center justify-center mt-3 border-2 border-amber-0 gap-2 h-[3rem] w-[8rem] bg-gray-800 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110  hover:bg-gray-950 active:scale-105 disabled:scale-100 disabled:bg-opacity-65"
+      disabled={pending}
+    >
+      {pending ? (
+        <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
+      ) : (
+        <>
+          Submit{" "}
+          <FaPaperPlane
+            className="text-xs opacity-70 transition-all 
+            group-hover:translate-x-1 
+            group-hover:-translate-y-1
+    "
+          />
+        </>
+      )}
+    </button>
+  );
+}
